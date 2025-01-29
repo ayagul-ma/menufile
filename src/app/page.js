9d2d4a0e5baed0231743b9d43bb71cc9ad21c7e1
24LP0954 24LP0954 <24LP0954@G303-CC0148.local>
2025-01-29 13:07:09 +0800

    code

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,147 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+const menu = [
+  {
+    categoryName:"Salads",
+    foods: {
+      foodName: 'CHIKEN CAESER SALAD',
+      ingredients: 'Served with Sundried tomatoes , chunkly garlic croutons and grilled Chiken',
+      price: 24900,
+
+    }
+  },
+  {
+    foods: {
+      foodName: 'GREK SALAD',
+      ingredients: 'fresh cucumbers, tomatoes, red onions, Kalamata olives, and feta  cheese, drizzled with olive oil.',
+      price: 21900,
+
+    }
+  },
+  {
+    foods: {
+      foodName: 'QUINOA POWER SALAD',
+      ingredients: 'A healthy  mix of quinoa, avocado, roasted sweet pototoes, and kale with a tangy leon dressing ',
+      price: 21900,
+
+    }
+  },
+  {
+    categoryName:"Burger",
+    foods: {
+      foodName: 'Classic burger',
+      ingredients: 'Juicy beef patty, cheddar cheese, lettuce, tomato, and house-made sauce, served with fries',
+      price: 18900,
+
+    }
+  },
+  {
+    foods: {
+      foodName: 'BBQ BACON BURGER',
+      ingredients: 'fresh cucumbers, tomatoes, red onions, Kalamata olives, and feta  cheese, drizzled with olive oil.',
+      price: 21900,
+
+    }
+  },
+  {
+    foods: {
+      foodName: 'VEGAN BLACK BEAN BURGER',
+      ingredients: 'A healthy  mix of quinoa, avocado, roasted sweet pototoes, and kale with a tangy leon dressing ',
+      price: 17900,
+
+    }
+  },
+  {
+    categoryName:"PASTA",
+    foods: {
+      foodName: 'SPAGHETTI PASTA',
+      ingredients: 'Classic italian pasta with creamy egg sauce, Parmesan cheese, and crsppy pancetta',
+      price: 20900,
+
+    }
+  },
+  {
+    foods: {
+      foodName: 'SEAFOOD MARINARA',
+      ingredients: 'fresh cucumbers, tomatoes, red onions, Kalamata olives, and feta  cheese, drizzled with olive oil.',
+      price: 23500,
+
+    }
+  },
+  {
+    foods: {
+      foodName: 'PESTO CHICKEN PASTA',
+      ingredients: 'A healthy  mix of quinoa, avocado, roasted sweet pototoes, and kale with a tangy leon dressing ',
+      price: 21000,
+
+    }
+  },
+  {
+    categoryName:"PIZZA",
+    foods: {
+      foodName: 'MARGHERITI PIZZA',
+      ingredients: 'Simple and classsic with tomato sauce, fresh mozzarella, and basil leaves',
+      price: 30900,
+
+    }
+  },
+  {
+    foods: {
+      foodName: 'PEPPERONI FEAST',
+      ingredients: 'Loaded with pepperioni and melted mozzarella on a crispy crust',
+      price: 34500,
+
+    }
+  },
+  {
+    foods: {
+      foodName: 'VEGETARIAN DELIGHT',
+      ingredients: 'Topped with bell peppers, mushrooms, olives, onions, and fresh tomatoes ',
+      price: 29900,
+
+    }
+  },
+]
+export default function Menu({categoryName,foods, foodName, ingredients, price}) {
+  return(
+    <div>
+      <div>
+        <h1 className="title">pronxo</h1>
+        <div className="card-container">
+          <div className="category">
+            <p>SALADS</p>
+          </div>
+            <div className="food">
+              <div className="food-header">
+                <h6>CHICKEN CAESER SALAD</h6>
+                <p>20900</p>
+              </div>
+              <div>
+                <p>Served with Sundried tomatoes , chunkly garlic croutons and grilled Chiken</p>
+              </div>
+            </div>
+            <div className="food">
+              <div className="food-header">
+                <h6>GREEK SALAD</h6>
+                <p>21900</p>
+              </div>
+              <div>
+                <p>fresh cucumbers, tomatoes, red onions, Kalamata olives, and feta  cheese, drizzled with olive oil</p>
+              </div>
+            </div>
+            <div className="food">
+              <div className="food-header">
+                <h6>QUINOA POWER SALAD</h6>
+                <p>22500</p>
+              </div>
+              <div>
+                <p>A healthy  mix of quinoa, avocado, roasted sweet pototoes, and kale with a tangy leon dressing</p>
+              </div>
+            </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+     </div>
     </div>
   );
 }
